9604416a1b28c16b05f6b9ee4420025bee38a55d
Lakshit Ladha <122150975+Lakshitladha007@users.noreply.github.com>
2025-01-22 17:30:08 +0530

    Updated mergeSortedLL.c

--- mergeSortedLL.c
+++ mergeSortedLL.c
@@ -7,15 +7,15 @@ typedef struct Node{
     struct Node *prev;
 }Node;
 
-int main(){
-    int n1;
-    printf("enter number of nodes in list1:\n");
-    scanf("%d", &n1);
-    Node* head1=NULL;
-    Node* temp1=NULL;
-    Node* back1=NULL;
+Node* createLL(){
+    int n;
+    printf("enter number of nodes in list:\n");
+    scanf("%d", &n);
+    Node* head=NULL;
+    Node* temp=NULL;
+    Node* back=NULL;
     int flag=1;
-    while(n1--){
+    while(n--){
        int val;
        printf("Enter value of element\n");
        scanf("%d", &val);
@@ -24,48 +24,43 @@ int main(){
          curr->data=val;
          curr->next=NULL;
          curr->prev=NULL;
-         head1=curr;
-         temp1=curr;
+         head=curr;
+         temp=curr;
          flag=0;
        } else{
          curr->data=val;
          curr->next=NULL;
-         curr->prev=temp1;
-         temp1->next=curr;
-         temp1=curr;
+         curr->prev=temp;
+         temp->next=curr;
+         temp=curr;
        }
     }
-    
-    int n2;
-    printf("enter number of nodes in list2:\n");
-    scanf("%d", &n2);
+    return head;
+}
 
-    Node* head2=NULL;
-    Node* temp2=NULL;
-    while(n2--){
-        int val;
-        printf("Enter value of element\n");
-        scanf("%d", &val);
-        Node* curr= (Node*)malloc(sizeof(Node));
-        if(flag){
-        flag=0;
-        head2=curr;
-        curr->data=val;
-        curr->next=NULL;
-        temp2=head2;
-        } else{
-         curr->data=val;
-         curr->next=NULL;
-         temp2->next=curr;
-         temp2=curr;
-        }
+int findLength(Node* head){
+    Node* temp=head;
+    int count=0;
+    while(temp!=NULL){
+        count++;
+        temp=temp->next;
     }
+    return count;
+}
+
+void printList(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        printf("%d ", temp->data);
+        temp=temp->next;
+    }
+}
+
+void mergeList(Node* head1, Node* head2, int* arr){
     
-    temp1=head1;
-    temp2=head2;
-    int len=n1+n2;
-    int* arr= (int*)calloc(len, sizeof(int));
     int index=0;
+    Node* temp1=head1;
+    Node* temp2=head2;
     while(temp1!=NULL && temp2!=NULL){
         if(temp1->data<=temp2->data){
             arr[index++]=temp1->data;
@@ -76,16 +71,40 @@ int main(){
         }
     }
     while(temp1!=NULL){
-        arr[index++]=temp->data;
+        arr[index++]=temp1->data;
         temp1=temp1->next;
     }
     while(temp2!=NULL){
-        arr[index++]=temp->data;
-        temp1=temp1->next;
+        arr[index++]=temp2->data;
+            temp2=temp2->next;
+    }
+   
+}
+
+int main(){
+   Node* head1=createLL();
+   Node* head2=createLL();
+   
+    int len= findLength(head1)+findLength(head2);
+    int* arr= (int*)calloc(len, sizeof(int));
+    mergeList(head1, head2,arr);
+    
+   // joining bot the lists
+   Node* temp=head1;
+   while(temp->next!=NULL){
+       temp=temp->next;
+   }
+   temp->next=head2;
+   head2->prev=temp;
+    temp=head1;
+    
+    int index=0;
+    // copying array data into list
+    while(temp!=NULL){
+        temp->data=arr[index++];
+        temp=temp->next;
     }
     
-    for(int i=0; i<len; i++){
-        printf("%d ", arr[i]);
-    }
+    printList(head1);
     return 0;
 }
